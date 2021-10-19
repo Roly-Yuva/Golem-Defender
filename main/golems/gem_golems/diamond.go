44e7464758077e7f87bae4613be4bff4bfc75e52
Roly-Yuva <codedyuva333@gmail.com>
2021-10-18 20:55:25 -0400

    created multiple files

---
+++ main/golems/gem_golems/diamond.go
@@ -0,0 +1,15 @@
+components {
+  id: "stats"
+  component: "/src/scripts/gem_golem_scripts/diamond_stats.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ main/golems/gem_golems/peridot.go
@@ -0,0 +1,15 @@
+components {
+  id: "stats"
+  component: "/src/scripts/gem_golem_scripts/peridot_stats.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
